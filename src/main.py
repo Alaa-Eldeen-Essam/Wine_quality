5562e4b06bd9942ddf439e196398f089ef1155c4
Alaa1299 <axxxaa990@gmail.com>
2025-10-20 20:52:51 +0300

    connect static

--- src/main.py
+++ src/main.py
@@ -22,9 +22,9 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory=".", html=True), name="static")
 
 # Serve index.html at root
-# @app.get("/")
-# async def serve_index():
-#     return FileResponse("index.html")
+@app.get("/")
+async def serve_index():
+    return FileResponse("static/index.html")
 
 # CORS - Allow Railway domain
 app.add_middleware(
